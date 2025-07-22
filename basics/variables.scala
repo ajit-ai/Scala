a11ad9ec6e670ce7ba6fc84a665f12eaa131f582
ajitj <ajitk123@gmail.com>
2025-07-22 13:36:59 +0530

    updates scala

---
+++ basics/variables.scala
@@ -0,0 +1,13 @@
+object variables {
+    def main(args: Array[String]): Unit = {
+    
+        println(1) // 1
+        println(1 + 1) // 2
+        println("Hello!") // Hello!
+        println("Hello," + " world!") // Hello, world!
+
+        val x = 1 + 1
+        println(x) // 2
+
+    }
+}
